85f63d0d3e4e87962ff5e7d188ba04de05c031a2
skahol <44722358+skahol@users.noreply.github.com>
2020-09-01 17:30:17 +0530

    updated

--- calcu/Calculator.py
+++ calcu/Calculator.py
@@ -1,47 +1,45 @@
-import tkinter as tk
-mainWindow=tk.Tk()
-mainWindow.title("CALCULATOR")
-heading_label=tk.Label(mainWindow,text="Calculator",padx=(10),pady=(20))
-heading_label.pack()
-heading_label=tk.Label(mainWindow,text="ENTER FIRST NUMBER")
-heading_label.pack()
-first_field=tk.Entry(mainWindow)
-first_field.pack()
-heading_label=tk.Label(mainWindow,text="ENTER SECOND NUMBER")
-heading_label.pack()
-second_field=tk.Entry(mainWindow)
-second_field.pack()
-operation=tk.Label(mainWindow,text="operator")
-operation.pack()
-def addition():
-    first=int(first_field.get())
-    second=int(second_field.get())
-    result=first+second
-    result_label.config(text="operation result is:" +str(result))
-addition_button=tk.Button(mainWindow,text="+",command=lambda:addition())
-addition_button.pack()
-def minus():
-    first=int(first_field.get())
-    second=int(second_field.get())
-    result=first-second
-    result_label.config(text="operation result is:" +str(result))
+def add(x, y):
+    return x + y
 
-minus_button=tk.Button(mainWindow,text="-", command=lambda:minus())
-minus_button.pack()
-def multiply():
-    first=int(first_field.get())
-    second=int(second_field.get())
-    result=first*second
-    result_label.config(text="operation result is:" +str(result))
-mul_button=tk.Button(mainWindow,text="*", command=lambda:multiply())
-mul_button.pack()
-def divide():
-    first=int(first_field.get())
-    second=int(second_field.get())
-    result=first/second
-    result_label.config(text="operation result is:" +str(result))
-divide_button=tk.Button(mainWindow,text="/", command=lambda:divide())
-divide_button.pack()
-result_label=tk.Label(mainWindow, text="operations result is:")
-result_label.pack()
-mainWindow.mainloop()
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
