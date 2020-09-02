90e7c3a2e07fa906ed9173fbbc19ab998fe429fe
skahol <skahol2000@gmail.com>
2020-09-02 10:21:16 +0530

    updated

--- calcu/Calculator.py
+++ calcu/Calculator.py
@@ -13,33 +13,36 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+def lambda_handler(event, context):    
+    print("Select operation.")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+    while True:
+        # Take input from the user
+        print("Enter choice(1/2/3/4): ")
+        choice = event['choice']
 
-while True:
-    # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+        # Check if choice is one of the four options
+        if choice in ('1', '2', '3', '4'):
+            print("Enter first number: ")
+            num1 = event['f_num']
+            print("Enter second number: ")
+            num2 = event['s_num']
 
-    # Check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
-    else:
-        print("Invalid Input")
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+            break
+        else:
+            print("Invalid Input")
